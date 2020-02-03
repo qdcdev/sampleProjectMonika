d1eb28dfee5013c7fd60454fdc57e2774742f0fe
eyad al qudsi <eyadalqudsi@eyads-MacBook-Pro.local>
2020-02-03 13:09:44 +0400

    my first commit here

---
+++ sampleProjectMonika/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sampleProjectMonika
+//
+//  Created by eyad al qudsi on 2/3/20.
+//  Copyright © 2020 eyad al qudsi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
