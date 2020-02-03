63fd2fc1f9790c3fedd2602f72d3bd644b04bb0c
monika mohan <monikamohan2015@gmail.com>
2020-02-03 13:52:58 +0400

    next changes

--- sampleProjectMonika/ViewController.swift
+++ sampleProjectMonika/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("23456765")
+        print("new changess")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
